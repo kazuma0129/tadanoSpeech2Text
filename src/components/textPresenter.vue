b390594703b20d1667c4b208616c4fa3584a5b74
KazumaOhashi <b1016095@fun.ac.jp>
2019-09-18 01:03:41 +0900

    :tada: り

--- src/components/textPresenter.vue
+++ src/components/textPresenter.vue
@@ -1,12 +1,96 @@
 <template>
-  <div></div>
+  <v-container fluid>
+    <v-col col="12">
+      <v-row justify="center">
+        <h1 class="font-weight-thin">{{ text }}</h1>
+      </v-row>
+      <v-row justify="center">
+        <v-btn
+          large
+          outlined
+          color="primary"
+          v-show="!state"
+          @click="startSpeech"
+          >️️️⚡️go⚡️</v-btn
+        >
+        <v-btn large outlined color="warning" v-show="state" @click="stopSpeech"
+          >✋stop✋</v-btn
+        >
+      </v-row>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
+/* eslint-disable no-console */
+
 export default {
   name: "textPresenter",
-  data: function() {},
-  methods: function() {},
-  watch: function() {}
+  data: function() {
+    return {
+      state: Boolean,
+      text: null,
+      // eslint-disable-next-line no-undef
+      recognition: new webkitSpeechRecognition()
+    };
+  },
+  props: {
+    font: String
+  },
+  created: function() {
+    this.speechRecognize();
+    this.state = false;
+  },
+  watch: {
+    state: function() {
+      if (!this.state) {
+        this.stopSpeech();
+      }
+    }
+  },
+  methods: {
+    speechRecognize: function() {
+      this.recognition.onstart = () => {
+        console.log("onstart");
+      };
+      this.recognition.onspeechstart = () => {
+        console.log("onspeechstart");
+      };
+      this.recognition.onspeechend = () => {
+        console.log("onspeechend");
+      };
+      this.recognition.onend = () => {
+        if (this.state) {
+          this.startSpeech();
+        } else {
+          this.stopSpeech();
+        }
+        console.log("onend");
+      };
+      this.recognition.onresult = event => {
+        let interimTranscript = ""; // 暫定(灰色)の認識結果
+        let finalTranscript = ""; // 確定した(黒の)認識結果
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+          let transcript = event.results[i][0].transcript;
+          if (event.results[i].isFinal) {
+            finalTranscript += transcript;
+          } else {
+            interimTranscript = transcript;
+          }
+          this.text = finalTranscript + interimTranscript;
+        }
+      };
+    },
+    startSpeech: function() {
+      this.recognition.lang = "ja-JP";
+      this.recognition.interimResults = true;
+      this.state = true;
+      this.recognition.start();
+    },
+    stopSpeech: function() {
+      this.state = false;
+      this.recognition.stop();
+    }
+  }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,98 +1,30 @@
 <template>
-  <div></div>
+  <v-container fluid fill-height>
+    <v-col col="12">
+      <v-row align="center" justify="center">
+        <textPresenter :font="font"></textPresenter>
+      </v-row>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-import {
-  fontSelecter,
-  startButton,
-  stopButton,
-  textPresenter
-} from "@/components";
-
-navigator.getUserMedia =
-  navigator.getUserMedia ||
-  navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia;
+import textPresenter from "../components/textPresenter";
 
 export default {
   name: "home",
+  components: {
+    textPresenter
+  },
   data: function() {
     return {
-      status: "",
-      isDuration: false,
-      text: null,
-
-      // eslint-disable-next-line no-undef
-      recognition: new webkitSpeechRecognition(),
-      recognitionText: "Start",
-      notes: []
+      font: String
     };
   },
-  watch: {
-    status: function() {
-      if (this.status === "onend") {
-        this.startSpeech();
-      }
-    }
-  },
-  created: function() {
-    this.speechRecognize();
-  },
-  methods: {
-    speechRecognize: function() {
-      this.recognition.onstart = () => {
-        this.status = "onstart";
-        console.log(this.status);
-        this.isDuration = true;
-        this.recognitionText = "音声入力中...";
-      };
-      this.recognition.onspeechstart = () => {
-        this.status = "onspeechstart";
-        console.log(this.status);
-      };
-      this.recognition.onspeechend = () => {
-        this.status = "onspeechend";
-        console.log(this.status);
-      };
-      this.recognition.onend = () => {
-        this.status = "onend";
-        console.log(this.status);
-        // this.recognitionText = "Start";
-      };
-      this.recognition.onresult = event => {
-        this.status = "onresult";
-        console.log(this.status);
-        let interimTranscript = ""; // 暫定(灰色)の認識結果
-        let finalTranscript = ""; // 確定した(黒の)認識結果
-        for (let i = event.resultIndex; i < event.results.length; i++) {
-          let transcript = event.results[i][0].transcript;
-          if (event.results[i].isFinal) {
-            finalTranscript += transcript;
-          } else {
-            interimTranscript = transcript;
-          }
-          this.text = finalTranscript + interimTranscript;
-        }
-      };
-    },
-    startSpeech: function() {
-      this.status = "startSpeech";
-      console.log(this.status);
-      this.isDuration = true;
-      this.recognition.lang = "ja-JP";
-      this.recognition.interimResults = true;
-      this.recognition.start();
-    },
-    stopSpeech: function() {
-      this.status = "stopSpeech";
-      console.log(this.status);
-      this.recognition.stop();
-      this.status = "stop";
-      console.log(this.status);
-    }
-  }
+  created: function() {},
+  watch: {},
+  methods: {}
 };
 </script>
